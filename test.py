f81ea76d37bc233af7618dd07d39ba303fd7ba09
Gabe Stanton <acninetyfive@gmail.com>
2019-05-19 23:44:36 -0400

    Fixed a couple erroneous pseudo-legal moves

---
+++ test.py
@@ -0,0 +1,27 @@
+from player import Player
+from board import Board
+from piece import Piece
+
+g = Player("g", 1)
+b = Board()
+pcs = g.get_pieces()
+
+b.board[7,7] = 1
+
+
+
+for p in pcs:
+	moves = pcs[p].get_legal_moves()
+	for m in moves:
+		pcs[p].reset()
+		for c in m:
+			if c == 'r':
+				pcs[p].rotate()
+			elif c == 'f':
+				pcs[p].flip()
+		for i in moves[m]:
+			if not b.valid_move(pcs[p],7+i[0],7+i[1]):
+				print (p, m, i)
+				print(pcs[p].get_shape())
+
+print("Done")
