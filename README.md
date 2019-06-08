2b63998f55f755ff69bdd52616d2be3dfc8e333b
kewlkevkev <kewlkevkev@gmail.com>
2019-06-08 16:37:00 -0400

    cleanup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Blokus Bot Battleground
+
+Owner: Gabe Stanton
+
+## Requirements
+
+- python 3.*.*
+
+## Install Dependencies
+
+`python setup.py install`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Blokus',
+    install_requires=[
+        'pygame',
+        'numpy',
+        'scipy',
+    ]
+)
